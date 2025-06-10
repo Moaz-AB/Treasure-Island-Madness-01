e5343a998ec063e3ec6c09915765d59b2b62fc68
Moaz Abuzaid <moazabuzaid9@gmail.com>
2025-06-09 21:03:33 -0400

    Create Treasure-Island-Madness-01.py
    Enjoy the game!

---
+++ Treasure-Island-Madness-01.py
@@ -0,0 +1,323 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island!")
+print("You are a young pirate on a ship called the Wings of the Sea")
+ship=input("Would You like to see the ship? Type Y for Yes and N for No")
+if ship == "Y":
+    print('''#############################################################################
+#(@@@@)                    (#########)                   (@@@@@@@@(@@@@@@@@@#
+#@@@@@@)___                 (####)~~~   /\                ~~(@@@@@@@(@@@@@@@#
+#@@@@@@@@@@)                 ~~~~      /::~-__               ~~~(@@@@@@@@)~~#
+#@@@)~~~~~~                           /::::::/\                  ~~(@@@@)   #
+#~~~                              O  /::::::/::~--,                 ~~~~    #
+#                                 | /:::::/::::::/{                         #
+#                 |\              |/::::/::::::/:::|                        #
+#                |:/~\           ||:::/:::::/::::::|                        #
+#               |,/:::\          ||/'::: /::::::::::|                       #
+#              |#__--~~\        |'#::,,/::::::::: __|   ,,'`,               #
+#             |__# :::::\       |-#"":::::::__--~~::| ,'     ',     ,,      #
+#,    ,,     |____#~~~--\,'',.  |_#____---~~:::::::::|         ',','  ',    #
+# '.,'  '.,,'|::::##~~~--\    `,||#|::::::_____----~~~|         ,,,     '.''#
+#____________'----###__:::\_____||#|--~~~~::::: ____--~______,,''___________#
+#^^^  ^^^^^   |#######\~~~^^O, | ### __-----~~~~_____########'  ^^^^  ^^^   #
+#,^^^^^','^^^^,|#########\_||\__O###~_######___###########;' ^^^^  ^^^   ^^ #
+#^^/\/^^^^/\/\^^|#######################################;'/\/\/^^^/\/^^^/\/^#
+#   /\/\/\/\/\  /\|####################################'      /\/\/\/\/\    #
+#\/\/\     /\/\/\  /\/\/\/\    /\/\/\/\/\   /\/\/\    /\/\/\/\      /\/\/\/\#
+#spb\/\/\    /\/\/\/\    /\/\/\/\    /\/\/\/\   /\/\/\/\    /\/\/\/\/\      #
+#############################################################################''')
+else:
+    print("Damn...it was a good ship. You missed out friend")
+
+print("Your captain's name is yellow eye")
+print('''   _____
+              .-" .-. "-.
+            _/ '=(0.0)=' \_
+          /`   .='|m|'=.   `\
+          \________________ /
+      .--.__///`'-,__~\\\\~`
+     / /6|__\// a (__)-\\\\
+     \ \/--`((   ._\   ,)))
+     /  \\  ))\  -==-  (O)(
+    /    )\((((\   .  /)))))
+   /  _.' /  __(`~~~~`)__
+  //"\\,-'-"`   `~~~~\\~~`"-.
+ //  /`"              `      `\
+//''')
+print("....that's his name for some reason")
+say=input("Would you like to say something to the captain? Type Y for Yes and N for No")
+if say == "Y":
+    word = input("Your options: 1. Good Morning, Captain! 2. Why do they call you the yellow eye, Captain? 3. Are you scared of the sea, Captain? Pick 1, 2, or 3")
+    if word == "1":
+        print("Good Morning Son! Arrrgh!")
+    if word == "2":
+        print("BOY! That's a long story and the developer is too lazy to write it out..But let's say I got poisoned on a secret mission...Don't tell anyone or the sharks will be having a nice dinner tonight")
+else:
+    print("AM I SCARED OF THE SEA???? Are you stupid son? I own the 7 seas! Yo boys get this idiot out of my sight!")
+print("Yeah...Our captain is just the best")
+print("OH....")
+print("I forgot to introduce myself. I am Kale your Narrator/ Bestie")
+print("Kale: Let's check our bedrooms")
+print(''' ________________________________________
+               |%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%| o(=(=(=(=)=)=)=)o
+    _!_         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   !!!!!!!!!!!!!!! 
+   |~~~|        !!!!!!!!!!!!!!!!!}`   `{!!!!!!!!!!!!!!!!   !!!    ||   !!!
+   |___|        !!!!!!!!!!!!!!}`        `{!!!!!!!!!!!!!!   !!!    ||// !!!
+     |          !!!!!!!!!!!!} _          _`{!!!!!!!!!!!!   !!!    ||   !!!
+  __(_)______   !!!!!!!!!!}` /_\        /_\ `{!!!!!!!!!!   !!!    ||   !!!
+  |____=____|   !!!!!!!!!}   =|=        =|=   {!!!!!!!!!   !!!   ~||~  !!!
+  |____=____|   !!!!!!!}`__________  __________`{!!!!!!!   !!!    ||   !!! 
+  |____=____|   )~;@;~(|{__________}{__________}|)~;@;~(   !!! // ||   !!!
+  |____=____|   !!!!!!}|/%%%%%%%%%%%%%%%%%%%%%%\|{!!!!!!   !!!    ||   !!!
+__|____=____|___!!!!!!}/%%%%%%%%%%%%%%%%%%%%%%%%\{!!!!!!___!!!____||___!!!__
+  |/       \|   !!!!!!}%%%%%%%%%%%%%%%%%%%%%%%%%%{!!!!!!               
+                !!!!!!}%%%%%%%%%%%%%%%%%%%%%%%%%%{!!!!!!
+    `===========!!!!!!}%%%%%%%%%%%%%%%%%%%%%%%%%%{!!!!!!===========`
+   `============!!!!!!}%%%%%%%%%%%%%%%%%%%%%%%%%%{!!!!!!============`     
+  `=============!!!!!!}%%%%%%%%%%%%%%%%%%%%%%%%%%{!!!!!!=============`
+ `==============!!!!!!}||||||||||||||||||||||||||{!!!!!!==============`
+`===============!!!!!!}||||||||||||||||||||||||||{!!!!!!===============`
+================!!!!!!}|||____________________|||{!!!!!!================`
+================!lc!!!}~~/        <**>        \~~{!!!!!!=================`
+========================/______________________\==========================`  
+=========================|/        {}        \|============================`
+=========================|  <*>          <*>  |=============================`
+=========================|____________________|==============================`
+==============================================================================`''')
+ask = input("Kale: Why in the seven seas is your bedroom so fancy??? type IDK or type the captain must like me")
+if ask == "IDK":
+    print("Kale: Well let's go ask Mr. Seaweed for our actual rooms")
+else:
+    print("SCRAM!!! Captain yellow eye: Who the hell allowed those rats in my bedroom?!?!?")
+    print("Kale: Let's run to Mr. Seaweed he probably knows where's your bedroom!")
+print("Kale: Hey Mr. Seaweed :)")
+print("Grrrrrrr")
+print('''     .^.,*.
+                    (   )  )
+                   .~       "-._   _.-'-*'-*'-*'-*'-'-.--._
+                 /'             `"'                        `.
+               _/'                                           `.
+          __,""                                                ).--.
+       .-'       `._.'                                          .--.\
+      '                                                         )   \`:
+     ;                                                          ;    "
+    :                                                           )
+    | 8                                                        ;
+     =                  )                                     .
+      \                .                                    .'
+       `.            ~  \                                .-'
+         `-._ _ _ . '    `.          ._        _        |
+                           |        /  `"-*--*' |       |  mb
+                           |        |           |       :
+ ~~~~~~~---   ~-~-~-~   -~-~-~-~-~-~~~~~~  ~~~~  ~-~-~-~-~-~-~-
+------~~~~~~~~~----------~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+ ~~~~~~~~~   ~~~~~~~~~       ~~~~~~~   ~~~~~~~~~  ~~~~~~~~~~~~~~~''')
+print("Kale: Yes...Mr. Seaweed is a hippo, but a really nice one!")
+print("Kale: Will you please, tell me where's.... bedroom is")
+name = input("Kale: My apologies, I never asked for your name. What should we call you?")
+print(f"Kale: So Mr. Seaweed can you tell me where will {name} be staying?")
+print("Mr. Seaweed: GRRRRRRRRRRRR!")
+print("Kale: Perfect! Second room on the left downstairs")
+print("Kale: Thankyou Mr. Seaweed!")
+print("Mr.Seaweed: grrrrr")
+print("On the way downstairs....")
+print("A crewmate pops from nowhere asking for your money!")
+print("Crewmate: BOY! Hand me all the allowance mommy gave you!")
+print('''
+                       __.------.                          
+                      (__  ___   )                         
+                        .)e  )\ /                          
+                       /_.------                           
+                       _/_    _/                           
+                   __.'  / '   `-.__                       
+                  / <.--'           `\                     
+                 /   \   \c           |                    
+                /    /    )  GoT  x    \                   
+                |   /\    |c     / \.-  \                  
+                \__/  )  /(     (   \   <>'\               
+                     / _/ _\-    `-. \/_|_ /<>             
+                    / /--/,-\     _ \     <>.`.            
+                    \/`--\_._) - /   `-/\    `.\           
+                     /        `.     /   )     `\          
+                     \      \   \___/----'                 
+                     |      /    `(                        
+ ___________         \    ./\_   _ \                       
+   ______________    /     |  )    '|                      
+ __________________ |     /   \     \     ___________a:f   
+                   /     |     |____.)                     
+                  /      \  a88a\___/88888a.               
+                 \_      :)8888888888888888888a.           
+                /` `-----'  `Y88888888888888888            
+                \____|         `88888888888P'              ''')
+crew = input("Type Y to give him all you money or N to say No, get out of my way!")
+if crew == "Y":
+    print("Crewmate: I am just joking, friend I just wanted to get to know you :)")
+else:
+    print("Crewmate: Ok....You're boring :/")
+print("Kale: Don't mind him. He's always bored")
+print("Kale: Here's your bedroom friend")
+print(''' .    !__________!    .    _______
+    /_\   |____  ____|   /_\   |__*__|
+   __|__  {____}{____}  __|__  |__*__|
+ __|_*_|__%%%%%%%%%%%%__|_*_|__|__*__|__
+   |   | %%%%%%%%%%%%%% |   |  |/   \|
+        %%%%%%%%%%%%%%%%
+       %%%%%%%%%%%%%%%%%%
+      %%%%%%%%%%%%%%%%%%%%
+     /||||||||||||||||||||\
+     ||||||||||||||||||lc||''')
+print("Kale: The captain makes sure to give us good rooms, so we can work as hard as we can")
+print("Kale: Get some rest. I will see you at dinner")
+print("AFTER SOME TIME...")
+print("Cook: TIME TO EAT YOU MONKEYS!")
+sleep = input(f"Kale: Oh hey {name}. Did you sleep well? Type Y for yes or N for No")
+if sleep == "Y":
+    print("Kale: That's good to hear that, my friend")
+else:
+    print("Kale: I am sorry to hear that. Did the rats bite your toes?")
+    print(f"{name}: RATS???")
+    print("Kale: Oh yes. They're very friendly when they're not hungry")
+food = input("Kale: Are you Hungry? type Y for Yes. Come on you gotta eat")
+if food == "Y":
+    print("Here you go! Fish soup.")
+    print('''           ___..)--------)-----..___
+       _.--"""""..--(---""""(""""-)---.. """".---.
+      '-.__   `"--......_________.....--"`  (     \_
+           """":---.._______________..---:"" ``"""-.`'-._
+                '--.._______________..--'           `'-._`'-._
+                                                         `'-._: wkm
+''')
+else:
+    print("Kale: Just come! Don't be boring")
+print("After dinner the ship arrived at the island and found a crazy tree!")
+tree = input("There's a weird mark under the tree. Dig there? Type Y for Yes or N for No")
+if tree == "Y":
+        print("Congrats you Win! I just wanted to make it easy for you :)")
+else:
+    print("Damn...You really have trust issues. Oh well GGS")
+print("Wait you thought we were done?")
+print("You open the chest and you find it full of bones!")
+print(''' .-.               .-.
+     (   `-._________.-'   )
+      >=     _______     =<
+ jgs (   ,-'`       `'-,   )
+      `-'               `-'
+      ''')
+think = input(f"Kale: {name}! What did you find? Tell the truth  type T or lie to Kale type L")
+if think == "T":
+    print(f"{name}: I found a bunch of bones in the chest")
+    print("Kale: Let's go tell captain!")
+    print("Captain: This is a warning sign from the natives! We must evacuate the Island!")
+    print("You were saved!")
+    print(f"Kale: Good job finding that box, {name}!")
+    print("Kale: But wait....what is that???")
+    print(f"{name}: What's what?")
+    print("Kale: There is a map between the bones!")
+    print("TO BE CONTINUED...TREASURE ISLAND 2")
+else:
+    print(f"{name}: The chest is empty")
+    print("Kale: Oh ok..")
+    print("You get attacked by the natives and the whole crew dies and the natives take you hostage!")
+    print("You are put next to a pile of skulls")
+    print(''' uuuuuuuuuuuuuuuuuuuuu.
+                   .u$$$$$$$$$$$$$$$$$$$$$$$$$$W.
+                 u$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Wu.
+               $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+              $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+         `    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+           .i$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+           $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$W
+          .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$W
+         .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+         #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$.
+         W$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$u       #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$~
+$#      `"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$i        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$        #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$         $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#$.        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+ $$      $iW$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$!
+ $$i      $$$$$$$#"" `"""#$$$$$$$$$$$$$$$$$#""""""#$$$$$$$$$$$$$$$W
+ #$$W    `$$$#"            "       !$$$$$`           `"#$$$$$$$$$$#
+  $$$     ``                 ! !iuW$$$$$                 #$$$$$$$#
+  #$$    $u                  $   $$$$$$$                  $$$$$$$~
+   "#    #$$i.               #   $$$$$$$.                 `$$$$$$
+          $$$$$i.                """#$$$$i.               .$$$$#
+          $$$$$$$$!         .   `    $$$$$$$$$i           $$$$$
+          `$$$$$  $iWW   .uW`        #$$$$$$$$$W.       .$$$$$$#
+            "#$$$$$$$$$$$$#`          $$$$$$$$$$$iWiuuuW$$$$$$$$W
+               !#""    ""             `$$$$$$$##$$$$$$$$$$$$$$$$
+          i$$$$    .                   !$$$$$$ .$$$$$$$$$$$$$$$#
+         $$$$$$$$$$`                    $$$$$$$$$Wi$$$$$$#"#$$`
+         #$$$$$$$$$W.                   $$$$$$$$$$$#   ``
+          `$$$$##$$$$!       i$u.  $. .i$$$$$$$$$#""
+             "     `#W       $$$$$$$$$$$$$$$$$$$`      u$#
+                            W$$$$$$$$$$$$$$$$$$      $$$$W
+                            $$`!$$$##$$$$``$$$$      $$$$!
+                           i$" $$$$  $$#"`  """     W$$$$
+                                                   W$$$$!
+                      uW$$  uu  uu.  $$$  $$$Wu#   $$$$$$
+                     ~$$$$iu$$iu$$$uW$$! $$$$$$i .W$$$$$$
+             ..  !   "#$$$$$$$$$$##$$$$$$$$$$$$$$$$$$$$#"
+             $$W  $     "#$$$$$$$iW$$$$$$$$$$$$$$$$$$$$$W
+             $#`   `       ""#$$$$$$$$$$$$$$$$$$$$$$$$$$$
+                              !$$$$$$$$$$$$$$$$$$$$$#`
+                              $$$$$$$$$$$$$$$$$$$$$$!
+                            $$$$$$$$$$$$$$$$$$$$$$$`
+                             $$$$$$$$$$$$$$$$$$$$"
+''')
+    rib = input("There's a rib next to you. Type U to use it or type L to leave it")
+    if rib == "U":
+        print("You managed to cut the rope and escape!")
+        print("While running you hit a small child from the village")
+        print("The child screams!")
+        soul = input("What will you do? Calm down the child? type C or Kidnap the child? type K")
+        if soul == "C":
+            print("The kid's mom shoot you with an arrow and take you to a nearby cave")
+            print("The mom: Ok, stranger I will help you get off this island!")
+            print(f"{name}: Thank you so much! But, why did you shoot me?")
+            print("The mom: I was just protecting my child, but he told me you were trying to calm him down.")
+            print("The mom: I will get you off this island. But, in one condition....")
+            print("To be continued.....Treasure Island 2")
+        else:
+            print("The kid's mom sees you and shoots you with an arrow...she killed you...")
+            print("Game Over")
+    else:
+        print("The natives bring you to their chief")
+        print("The chief sees that you are young and offers you a chance to live and work on the island")
+        life = input("Will you take the offer? Type Y for yes or N for No")
+        if life == "Y":
+            print(f"{name}: Thank you chief! I will gladly accept!")
+            print("The chief offer you one job.")
+            print("Chief: Son. Your job will be to raid any ship to lands on the island. You look like them, so they will trust you")
+            choice = input("Will you take the offer? Type Y for yes or N for No")
+            if choice == "Y":
+                print("Chief: Well...you better start training, then. You have to put some muscle on your bones to do such job")
+                print("To be continued...Treasure Island 2")
+            else:
+                print("Chief: Mmmm...interesting. Come closer, son.")
+                print("To be continued.....Treasure Island 2")
+        else:
+            print("Chief: Mmmm...interesting. Come closer, son.")
+            print("To be continued.....Treasure Island 2")
